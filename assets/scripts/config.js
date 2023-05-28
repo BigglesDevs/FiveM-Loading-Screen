0cb5f9269dbcb3989f67a14e89c0dbcd3f5b15ad
BigglesDevs <bigglesdevs2003@gmail.com>
2023-05-28 04:40:05 +0100

    first commit

---
+++ assets/scripts/config.js
@@ -0,0 +1,30 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "YOUR IP SERVER";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", label: "Discord", description: "Click here to copy the link and join our Discord server!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/sunriseroleplay"},
+//    {name: "instagram", label: "Instagram", description: "An Instagram page will open for us soon, feel free to join and follow us!", icon: "assets/media/icons/tiktok.png", link: "#"},
+    {name: "tebex", label: "Donations", description: "For donations, feel free to look at the room - #Donations at Discord.", icon: "assets/media/icons/tebex.png", link: "https://www.paypal.com/paypalme/SunriseCounty"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Staff list
+Config.Staff = [
+    {name: "Jacob.#1010 #1", description: "Owner", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1112166808800198717/1112218963053641739/a_60e2a4aaacc92ede500b33c25debba60.png"},
+    {name: "IOwnAFork#6969 #2", description: "Owner + Dev(TinyBit)", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1112166808800198717/1112203686031142944/fork.jpg"},
+
+
+];
+
+// Categories
+Config.Categories = [
+    {label: "Social Media", default: true},
+    {label: "Staff", default: false}
+];
+
+// Music
+Config.Song = "song.mp3";
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'GN_Biggles#7226'
+description 'nSCRP-loadingscreen'
+
+files {
+    '*.html',
+    'assets/**/*.*',
+    'assets/**/**/*.*'
+}
+
+client_script 'client.lua'
+
+--loadscreen_manual_shutdown "yes"
+loadscreen 'index.html'
